1ebed185538737620a3b1a08323b2118bdaac00d
Ana Dejanović <ana.dejanovich@gmail.com>
2023-04-16 22:03:17 +0200

    Added  top Artists and top Tracks fetch API

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -9,17 +9,41 @@ const Home = () => {
   const [topTracks, setTopTracks] = useState([]);
   const timeOfday = showTimeOfDay();
 
-  /*  useEffect(() => {
-    apiClient.get("artists/id/top-tracks").then((response) => {
-      setTopTracks(response.data);
+  useEffect((limit = 6, timeRange = "medium_term") => {
+    apiClient.get(`me/top/artists?limit=${limit}&time_range=${timeRange}`).then((response) => {
+      setTopArtist(
+        response.data.items.map((artist) => {
+          return {
+            image: artist.images[1].url,
+            name: artist.name,
+            type: artist.type,
+          };
+        }),
+      );
       console.log(response.data);
     });
-  }, []); */
+
+    apiClient.get(`me/top/tracks?limit=${limit}&time_range=${timeRange}`).then((response) => {
+      setTopTracks(
+        response.data.items.map((track) => {
+          return {
+            name: track.name,
+            album: track.album.name,
+            artist: track.artists.map((artist) => artist.name).join(" & "),
+            image: track.album.images[1].url,
+          };
+        }),
+      );
+      console.log(response.data);
+    });
+  }, []);
+
   return (
     <div className={styles.home_container}>
       <div className={styles.greeting}>
         <h1>Good {timeOfday}</h1>
       </div>
+      <div></div>
     </div>
   );
 };
