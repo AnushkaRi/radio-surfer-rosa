c0a383b641e091e39a82a27f9db6317e7db0dd46
Ana Dejanović <ana.dejanovich@gmail.com>
2023-04-27 22:27:24 +0200

    Added Artist page

--- src/pages/Artist/Artist.jsx
+++ src/pages/Artist/Artist.jsx
@@ -2,20 +2,97 @@ import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 
 import apiClient from "../../services/Spotify";
+import Card from "../../components/Card/Card";
+import CardGrid from "../../components/CardGrid/CardGrid";
+import { msToMinutesAndSeconds } from "../../services/Spotify";
 import styles from "./styles.module.css";
 
 const Artist = () => {
   const params = useParams();
-  const [artist, setArtist] = useState();
+  const [artist, setArtist] = useState([]);
+  const [tracks, setTracks] = useState([]);
+  const [albums, setAlbums] = useState([]);
 
   useEffect(() => {
     apiClient.get(`artists/${params.id}`).then((response) => {
-      setArtist(response);
+      setArtist(response.data);
+      console.log(response);
+    });
+
+    apiClient.get(`artists/${params.id}/top-tracks?market=US`).then((response) => {
+      setTracks(
+        response.data.tracks.map((track) => {
+          return {
+            id: track.id,
+            image: track.album.images[2].url,
+            name: track.name,
+            duration: track.duration_ms,
+          };
+        }),
+      );
+      console.log(response);
+    });
+
+    apiClient.get(`artists/${params.id}/albums?include_groups=album`).then((response) => {
+      setAlbums(
+        response.data.items.map((album) => {
+          return {
+            image: album.images[1].url,
+            title: album.name,
+            year: album.release_date.split("-")[0],
+            artist: album.artists[0].name,
+            id: album.id,
+          };
+        }),
+      );
       console.log(response);
     });
   }, []);
 
-  return <div className={styles.artist_container}></div>;
+  return (
+    <div className={styles.artist_container}>
+      <div className={styles.cover}>
+        <img src={artist.images?.[0].url} />
+        <div className={styles.info}>
+          <div className={styles.name}>{artist?.name}</div>
+        </div>
+      </div>
+
+      <div className={styles.tracks_container}>
+        <div className={styles.section_title}>Popular</div>
+        {tracks.map((track) => {
+          return (
+            <div className={styles.track_row}>
+              <div className={styles.track_details} key={track.id}>
+                <img src={track.image} />
+                <div className={styles.col}>
+                  <span className={styles.track_name}>{track.name}</span>
+                </div>
+              </div>
+              <div className={styles.col}>
+                <span>{msToMinutesAndSeconds(track.duration)}</span>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+
+      <div className={styles.album_container}>
+        <div className={styles.section_title}>Discography</div>
+        <CardGrid>
+          {albums.map((album) => (
+            <Card
+              key={album.id}
+              imageUrl={album.image}
+              title={album.title}
+              description={album.year}
+              name={album.artist}
+            />
+          ))}
+        </CardGrid>
+      </div>
+    </div>
+  );
 };
 
 export default Artist;
