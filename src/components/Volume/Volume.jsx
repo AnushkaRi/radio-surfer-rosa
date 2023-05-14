bd6ea2ad2d467fc9192ff251aba12d4865dd2c36
Ana Dejanović <ana.dejanovich@gmail.com>
2023-05-14 21:53:24 +0200

    Mute/unmute volume problem

--- src/components/Volume/Volume.jsx
+++ src/components/Volume/Volume.jsx
@@ -6,7 +6,7 @@ import styles from "./styles.module.css";
 
 const Volume = () => {
   const [volume, setVolume] = useState(50);
-  const [muted, setMuted] = useState(false);
+  const [isMuted, setIsMuted] = useState(false);
 
   const handleVolumeChange = async () => {
     apiClient.put(`me/player/volume?volume_percent=${volume}`).then((response) => {
@@ -14,31 +14,22 @@ const Volume = () => {
     });
   };
 
-  /*  const toggleMute = () => {
-    setMuted(!muted);
-  }; */
-
-  const handleUnmute = () => {
-    setMuted(!muted);
-    setVolume(50);
-    handleVolumeChange();
-  };
-
   const handleMute = () => {
-    setMuted(!muted);
-    setVolume(0);
+    if (isMuted) {
+      setVolume((prevVolume) => (prevVolume === 0 ? 50 : prevVolume));
+      setIsMuted(false);
+    } else {
+      setVolume(0);
+      setIsMuted(true);
+    }
     handleVolumeChange();
   };
 
   return (
     <div className={styles.volume_container}>
-      <div className={styles.volume_icon}>
-        {muted ? (
-          <FaVolumeMute color="white" onClick={handleUnmute} />
-        ) : (
-          <FaVolumeUp color="white" onClick={handleMute} />
-        )}
-        {muted ? <span className={styles.tooltip}>Unmute</span> : <span className={styles.tooltip}>Mute</span>}
+      <div className={styles.volume_icon} onClick={handleMute}>
+        {isMuted ? <FaVolumeUp color="white" /> : <FaVolumeMute color="white" />}
+        {isMuted ? <span className={styles.tooltip}>Mute</span> : <span className={styles.tooltip}>Unmute</span>}
       </div>
       <div className={styles.volume_slider}>
         <input
